79424514a54e379ca7872e934e219203a604acd2
VCastor <vcastorv@gmail.com>
2026-01-30 16:30:59 +0100

    cleaner updated

--- update.sh
+++ update.sh
@@ -7,17 +7,20 @@
 )
 
 # Convert RKF to XYZ
-$AMSBIN/plams rkf_to_xyz.py
+$AMSBIN/plams rkf_to_xyz.py 2> /dev/null
 
 # Select regions
-$AMSBIN/plams region_selector.py
+$AMSBIN/plams region_selector.py 2> /dev/null
 
 # Generate run files and update database
-$AMSBIN/plams run_generator.py
+$AMSBIN/plams run_generator.py 2> /dev/null
 
 # Send data to Rameau
 (
   cd run_scripts
   ./to_rameau.sh
 )
+
+# Cleanup
+rm -rf plams_workdir*
 
