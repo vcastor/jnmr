f556bc92592aeb5e361e4bd8b71d38dc97770675
VCastor <vcastorv@gmail.com>
2026-09-11 16:08:36 +0200

    small update for the scripting analysis [bash]

--- analysis.sh
+++ analysis.sh
@@ -63,9 +63,10 @@ run_plams gauche_anti    analysis/gauche_anti.py    clusters "$RKF_DIR"
 run_plams qtaim_analysis analysis/qtaim_analysis.py clusters "$RKF_DIR"
 run_plams choline_fold   analysis/choline_fold.py   clusters amsoutput/qtaim
 run_plams cl_environment analysis/cl_environment.py clusters amsoutput/qtaim
-run_plams nh2_environment analysis/nh2_environment.py clusters amsoutput/qtaim
+
+run_plams nh2_environment        analysis/nh2_environment.py clusters amsoutput/qtaim
 run_plams representative_cluster analysis/representative_cluster.py clusters
-run_plams interaction_matrix analysis/interaction_matrix.py clusters amsoutput/qtaim
+run_plams interaction_matrix     analysis/interaction_matrix.py clusters amsoutput/qtaim
 rm -rf plams_workdir*
 
 ./analysis/distance_plot.py
